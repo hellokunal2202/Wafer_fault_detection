e89cff8f3c8f175ebbfe043ded95e3adc70b2f94
Kunal Mali <malikunal2202@gmail.com>
2023-07-12 19:56:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.1",
+    author="Kunal_Mali",
+    author_email="malikunal2202@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
